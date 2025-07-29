e83033a681bfdd9f37663a7593a12b58cb93ed9e
Dharmatej Mallampati <143396853+dharma0504@users.noreply.github.com>
2025-07-29 11:06:05 +0530

    Delete backend directory

--- backend/routes/otpSessionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { sendOtp, verifyOtp } = require("../Controllers/otpSessionController");
-
-router.post("/request", sendOtp);   // /api/otp/request
-router.post("/verify", verifyOtp);  // /api/otp/verify
-
-module.exports = router;
--- backend/routes/update.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { updateUser } = require('../Controllers/updatecontroller');
-
-const router = express.Router();
-
-router.put('/update', updateUser);
-
-module.exports = router; // ✅ Export router directly
